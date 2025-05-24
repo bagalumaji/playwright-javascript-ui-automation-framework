533896a610728c20037928d7914c5615c0052fef
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-05-24 08:13:30 +0530

    updated test with env variables

--- tests/logintest.spec.js
+++ tests/logintest.spec.js
@@ -1,16 +1,26 @@
 import {test,expect} from '@playwright/test';
 import {LoginPage} from '../pages/loginpage';
 import {HomePage} from '../pages/homepage';
+import path from 'path';
+import dotenv from 'dotenv';
+dotenv.config({path:path.resolve(__dirname,'..','.env')});
+const baseUrl=process.env.baseURL
+const username=process.env.login_username
+const password=process.env.login_password
 
 test.beforeEach(async({page})=>{
-    await page.goto("/");
+    await page.goto(baseUrl);
 });
 
 test("login test",async({page})=>{
-   
+    console.log(baseUrl);
+    console.log(username);
+    console.log(password);
+    console.log("----------------------")
+
     const loginPage= new LoginPage(page);
     await loginPage.verifyLoginPage();
-    await loginPage.performLogin("standard_user","secret_sauce");
+    await loginPage.performLogin(username,password);
 
     const homePage = new HomePage(page);
     await homePage.verifyHomePage();
