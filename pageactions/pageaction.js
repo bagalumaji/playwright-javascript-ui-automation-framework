a4733655df659e37d18ff32bf8284ee34e76b899
umaji bagal <bagalumaji@gmail.com>
2025-05-25 09:56:24 +0530

    added Wait For Element ToBe Not Visible

--- pageactions/pageaction.js
+++ pageactions/pageaction.js
@@ -8,4 +8,8 @@ export class PageAction {
         } catch (error) {
         }
     }
+
+    async waitForElementToBeNotVisible(locator,timeout) {
+        await expect(locator).not.toBeVisible({ timeout: timeout })
+    }
 }
