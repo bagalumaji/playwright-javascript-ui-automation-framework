5c82afe80aeeb51c416cabd460d50cc22119a1be
umaji bagal <bagalumaji@gmail.com>
2025-05-26 12:38:40 +0530

    updated with timeout

--- pageactions/pageaction.js
+++ pageactions/pageaction.js
@@ -1,21 +1,24 @@
 import {expect} from "@playwright/test";
+import envtestdata from '../testdata/config.json';
 
 export class PageAction {
-    static async waitForElementToBeVisible(locator,timeout) {
-        await expect(locator).toBeVisible({ timeout:timeout });
+    static async waitForElementToBeVisible(locator,timeOut) {
+        await expect(locator).toBeVisible({ timeout: timeOut ?? envtestdata.wait.defaultTimeout });
         try {
-            await locator.scrollIntoViewIfNeeded({ timeout: timeout});
+            await locator.scrollIntoViewIfNeeded({ timeout: timeOut ?? envtestdata.wait.defaultTimeout });
         } catch (error) {
         }
     }
 
-    static async waitForElementToBeNotVisible(locator,timeout) {
-        await expect(locator).not.toBeVisible({ timeout: timeout })
+    static async waitForElementToBeNotVisible(locator,timeOut) {
+        await expect(locator).not.toBeVisible({ timeout: timeOut ?? envtestdata.wait.defaultTimeout })
     }
-    static async click(locator,timeout){
-        await locator.click(timeout)
+
+    static async click(locator,timeOut){
+            await locator.click({ timeout: timeOut ?? envtestdata.wait.defaultTimeout });
     }
-    static async fill(locator,text,timeout) {
-        await locator.fill(text,timeout)
+
+    static async fill(locator,text,timeOut) {
+        await locator.fill(text,{ timeout: timeOut ?? envtestdata.wait.defaultTimeout })
     }
 }
